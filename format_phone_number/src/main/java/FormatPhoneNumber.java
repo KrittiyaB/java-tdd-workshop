31af7da48930690d891fec7f3e795a2fd26c105c
Krittiya Boonteeranon <krittiya.bo@kasikornbank.com>
2017-07-20 16:25:34 +0700

    แก้ไข (Test Case oddPhoneNumberOnlyLast2Group) จัด format phone number ให้ 2 กลุ่มสุดท้ายเป็นกลุ่มละ 2 ตัว

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -1,32 +1,53 @@
 public class FormatPhoneNumber {
 
-    public String removeNonDigit(String input) {
-    	int length = input.length();
-    	String result = "";
-    	for(int i=0; i<length; i++) {
-    		if(input.charAt(i) != '-' && input.charAt(i) != ' ') {
-    			result += input.charAt(i); 
-    		}
-    	}
-        return result;
-    }
+	public String removeNonDigit(String input) {
+		int length = input.length();
+		String result = "";
+		for (int i = 0; i < length; i++) {
+			if (input.charAt(i) != '-' && input.charAt(i) != ' ') {
+				result += input.charAt(i);
+			}
+		}
+		return result;
+	}
 
-    public String solution(String input) {
-    	FormatPhoneNumber phone = new FormatPhoneNumber();
-    	String number = phone.removeNonDigit(input);
-    	int length = number.length();
-    	String resultFormat = "";
-    	
-    	for(int i=0; i<length; i++) {
-    		if(i>0 && i%3 == 0) {
-    			resultFormat += "-"+ number.charAt(i);
-    		}else {
-    			resultFormat += number.charAt(i);
-    		}
-    		
-    	}
-    	
-        return resultFormat;
-    }
+	public String solution(String input) {
+		FormatPhoneNumber phone = new FormatPhoneNumber();
+		String number = phone.removeNonDigit(input);
+		int lengthNumber = number.length();
+		String resultFormat = "";
+		System.out.println("length *" + lengthNumber);
+		if (lengthNumber % 3 == 1) {
+			int index = 0;
+			for (int i = 0; i < ((lengthNumber / 3) - 1) * 3; i += 3) {
+				resultFormat += number.substring(i, i + 3) + "-";
+				System.out.println(resultFormat);
+				index = i + 3;
+			}
+			System.out.println("index" + index);
+			for (int j = index; j < lengthNumber; j += 2) {
+				if (j + 2 > lengthNumber - 1) {
+					resultFormat += number.substring(j, j + 2);
+					System.out.println(j + "->" + resultFormat);
+				} else {
+					resultFormat += number.substring(j, j + 2) + "-";
+					System.out.println(j + "->$" + resultFormat);
+				}
+
+				System.out.println(resultFormat);
+			}
+
+		} else {
+			for (int i = 0; i < lengthNumber; i++) {
+				if (i > 0 && i % 3 == 0) {
+					resultFormat += "-" + number.charAt(i);
+				} else {
+					resultFormat += number.charAt(i);
+				}
+			}
+		}
+
+		return resultFormat;
+	}
 
 }
