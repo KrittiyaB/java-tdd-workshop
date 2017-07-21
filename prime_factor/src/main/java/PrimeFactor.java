6028a1b652be0db475b9e0126d59bcd5ba1ccbec
Krittiya Boonteeranon <krittiya.bo@kasikornbank.com>
2017-07-21 09:54:33 +0700

    แก้ไข Test case ที่ input เป็นเลขค่ 9, 27

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -7,21 +7,29 @@ public class PrimeFactor {
 
 	public List<Integer> of(int number) {
 
-		if (number == 2 || number == 3) {
+		if (number <= 1) {
+			return list;
+		} else if (number == 2 || number == 3) {
 			list.add(number);
 		} else {
 			if (number % 2 == 0) { // Even
 				number = number / 2;
 				list.add(2);
-
 				if (number == 2 || number == 3) {
 					list.add(number);
 				} else {
 					of(number);
 				}
-
+				
 			} else { // Odd
-
+				number = number / 3;
+				list.add(3);
+				if (number == 3) {
+					list.add(number);
+				} else {
+					of(number);
+				}
+				
 			}
 		}
 		return list;
