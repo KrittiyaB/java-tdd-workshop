1e20e9892e98d8fa5728dd61172927a108801522
Krittiya Boonteeranon <krittiya.bo@kasikornbank.com>
2017-07-21 08:50:00 +0700

    แก้ไข test case ที่ iput เป็น 2 หรือ 3

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -5,9 +5,9 @@ public class PrimeFactor {
     public List<Integer> of(int number) {
     	List<Integer> list = new ArrayList<Integer>();
     	
-    	if(number == 1) {
-    		return list;
+    	if(number == 2 || number == 3) {
+    		list.add(number);
     	}
-        return null;
+        return list;
     }
 }
--- prime_factor/src/test/java/PrimeFactorTest.java
+++ prime_factor/src/test/java/PrimeFactorTest.java
@@ -14,13 +14,13 @@ public class PrimeFactorTest {
         assertEquals(list(), primeFactor.of(1));
     }
 
-    @Test @Ignore
+    @Test 
     public void should_return_2_when_number_is_2() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(list(2), primeFactor.of(2));
     }
 
-    @Test @Ignore
+    @Test 
     public void should_return_3_when_number_is_3() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(list(3), primeFactor.of(3));
