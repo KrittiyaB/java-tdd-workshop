ad3ebcf6bce4e3a14a5577370f3054f922685740
krittiya.bo <krittiya.bo@K16SOF6XYRC2.kasikornbank.com>
2017-07-24 16:37:51 +0700

    test Git Commit with Eclipse

--- range/src/main/java/MyRange.java
+++ range/src/main/java/MyRange.java
@@ -1,19 +1,19 @@
 public class MyRange {
-	
+
 	public String of(String input) {
 		String result = "";
-		for(int i = getFirstNumber(input); i<=getLastNumber(input); i++) {
+		for (int i = getFirstNumber(input); i <= getLastNumber(input); i++) {
 			result += i;
 		}
-		
+
 		return result;
 	}
 
 	public int getLastNumber(String input) {
-		if(isก้ามปูปิด(input)) {
+		if (isก้ามปูปิด(input)) {
 			return Character.getNumericValue(input.charAt(3));
 		}
-		return Character.getNumericValue(input.charAt(3))-1;
+		return Character.getNumericValue(input.charAt(3)) - 1;
 
 	}
 
@@ -25,10 +25,10 @@ public class MyRange {
 	}
 
 	public int getFirstNumber(String input) {
-		if(isก้ามปูเปิด(input)) {
+		if (isก้ามปูเปิด(input)) {
 			return Character.getNumericValue(input.charAt(1));
 		}
-		return Character.getNumericValue(input.charAt(1))+1;
+		return Character.getNumericValue(input.charAt(1)) + 1;
 	}
 
 	public boolean isก้ามปูเปิด(String input) {
